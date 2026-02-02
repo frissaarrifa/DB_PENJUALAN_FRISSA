bff75400e95df05a0f4e95bed6733542716f0657
frissaarrifa <frissaarrifarachmawati@gmail.com>
2026-02-02 08:33:31 +0700

    Update file penjualan
    Mengubah dan menambahkan file

--- admin/penjualan_update.php
+++ admin/penjualan_update.php
@@ -2,12 +2,15 @@
 include '../koneksi.php';
 
 $id_jual     = $_POST['id_jual'];
+$id_barang   = $_POST['id_barang'];
 $total_harga = $_POST['total_harga'];
+$tgl_jual    = $_POST['tgl_jual'];
 
-mysqli_query($koneksi,"
-    UPDATE penjualan SET total_harga='$total_harga'
+mysqli_query($koneksi,"UPDATE penjualan SET
+    id_barang='$id_barang',
+    total_harga='$total_harga',
+    tgl_jual='$tgl_jual'
     WHERE id_jual='$id_jual'
 ");
 
 header("location:penjualan.php");
-?>
--- user/penjualan_edit.php
+++ user/penjualan_edit.php
@@ -2,6 +2,11 @@
 include 'header.php';
 include '../koneksi.php';
 
+if(!isset($_GET['id'])){
+    header("location:penjualan.php");
+    exit;
+}
+
 $id = $_GET['id'];
 $data = mysqli_query($koneksi,"SELECT * FROM penjualan WHERE id_jual='$id'");
 $d = mysqli_fetch_assoc($data);
@@ -18,9 +23,30 @@ $d = mysqli_fetch_assoc($data);
     <input type="hidden" name="id_jual" value="<?= $d['id_jual']; ?>">
 
     <div class="form-group">
+        <label>Barang</label>
+        <select name="id_barang" class="form-control" required>
+            <?php
+            $barang = mysqli_query($koneksi,"SELECT * FROM barang");
+            while($b = mysqli_fetch_assoc($barang)){
+                $selected = ($b['id_barang'] == $d['id_barang']) ? "selected" : "";
+                echo "<option value='$b[id_barang]' $selected>
+                        $b[nama_barang] | Harga: Rp ".number_format($b['harga_jual'])."
+                      </option>";
+            }
+            ?>
+        </select>
+    </div>
+
+    <div class="form-group">
         <label>Total Harga</label>
         <input type="number" name="total_harga" class="form-control"
                value="<?= $d['total_harga']; ?>" required>
+    </div>
+
+    <div class="form-group">
+        <label>Tanggal Jual</label>
+        <input type="date" name="tgl_jual" class="form-control"
+               value="<?= $d['tgl_jual']; ?>" required>
     </div>
 
     <input type="submit" value="Update" class="btn btn-primary">
