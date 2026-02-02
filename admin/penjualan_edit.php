4ac5de9f1246c3f98125fa695d9a30bb077498c7
frissaarrifa <frissaarrifarachmawati@gmail.com>
2026-02-02 08:57:14 +0700

    Update file penjualan admin dan kasir
    memperbarui file admin dan kasir

--- admin/penjualan_edit.php
+++ admin/penjualan_edit.php
@@ -2,58 +2,65 @@
 include 'header.php';
 include '../koneksi.php';
 
-if(!isset($_GET['id'])){
-    header("location:penjualan.php");
-    exit;
-}
-
 $id = $_GET['id'];
-$data = mysqli_query($koneksi,"SELECT * FROM penjualan WHERE id_jual='$id'");
-$d = mysqli_fetch_assoc($data);
+
+$penjualan = mysqli_query($koneksi,"
+    SELECT * FROM penjualan 
+    WHERE id_jual='$id'
+");
+$p = mysqli_fetch_assoc($penjualan);
 ?>
 
-<div class="container" style="margin-top:70px;">
-<div class="col-md-6 col-md-offset-3">
-<div class="panel panel-default" style="padding:20px;">
+<div class="container">
+    <br><br><br>
+    <div class="col-md-6 col-md-offset-3">
 
-<h4>Edit Penjualan</h4>
+        <div class="panel">
+            <div class="panel-heading">
+                <h4>Edit Data Penjualan</h4>
+            </div>
 
-<form method="POST" action="penjualan_update.php">
+            <div class="panel-body">
 
-    <input type="hidden" name="id_jual" value="<?= $d['id_jual']; ?>">
+                <form method="POST" action="penjualan_update.php">
 
-    <div class="form-group">
-        <label>Barang</label>
-        <select name="id_barang" class="form-control" required>
-            <?php
-            $barang = mysqli_query($koneksi,"SELECT * FROM barang");
-            while($b = mysqli_fetch_assoc($barang)){
-                $selected = ($b['id_barang'] == $d['id_barang']) ? "selected" : "";
-                echo "<option value='$b[id_barang]' $selected>
-                        $b[nama_barang] | Harga: Rp ".number_format($b['harga_jual'])."
-                      </option>";
-            }
-            ?>
-        </select>
+                    <input type="hidden" name="id_jual" value="<?php echo $p['id_jual']; ?>">
+                    <input type="hidden" name="user_id" value="<?php echo $p['user_id']; ?>">
+
+                    <div class="form-group">
+                        <label>Barang</label>
+                        <select name="id_barang" class="form-control" required>
+                            <?php
+                            $barang = mysqli_query($koneksi,"SELECT * FROM barang");
+                            while($b = mysqli_fetch_assoc($barang)){
+                                $selected = ($b['id_barang'] == $p['id_barang']) ? "selected" : "";
+                            ?>
+                                <option value="<?php echo $b['id_barang']; ?>" <?php echo $selected; ?>>
+                                    <?php echo $b['nama_barang']; ?> (Stok: <?php echo $b['stok']; ?>)
+                                </option>
+                            <?php } ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Jumlah</label>
+                        <input type="number" name="jumlah" class="form-control" min="1" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Tanggal Jual</label>
+                        <input type="date" name="tgl_jual" class="form-control" 
+                               value="<?php echo $p['tgl_jual']; ?>" required>
+                    </div>
+
+                    <br>
+                    <input type="submit" class="btn btn-primary" value="Update">
+                    <a href="penjualan.php" class="btn btn-default">Kembali</a>
+
+                </form>
+
+            </div>
+        </div>
+
     </div>
-
-    <div class="form-group">
-        <label>Total Harga</label>
-        <input type="number" name="total_harga" class="form-control"
-               value="<?= $d['total_harga']; ?>" required>
-    </div>
-
-    <div class="form-group">
-        <label>Tanggal Jual</label>
-        <input type="date" name="tgl_jual" class="form-control"
-               value="<?= $d['tgl_jual']; ?>" required>
-    </div>
-
-    <input type="submit" value="Update" class="btn btn-primary">
-    <a href="penjualan.php" class="btn btn-default">Kembali</a>
-
-</form>
-
-</div>
-</div>
 </div>
--- user/penjualan_update.php
+++ user/penjualan_update.php
@@ -1,15 +1,55 @@
 <?php
 include '../koneksi.php';
 
-$id_jual     = $_POST['id_jual'];
-$id_barang   = $_POST['id_barang'];
-$total_harga = $_POST['total_harga'];
-$tgl_jual    = $_POST['tgl_jual'];
+$id_jual   = $_POST['id_jual'];
+$id_barang = $_POST['id_barang'];
+$jumlah    = $_POST['jumlah'];
+$tgl_jual  = $_POST['tgl_jual'];
+$user_id   = $_POST['user_id'];
 
-mysqli_query($koneksi,"UPDATE penjualan SET
-    id_barang='$id_barang',
-    total_harga='$total_harga',
-    tgl_jual='$tgl_jual'
+$lama = mysqli_query($koneksi,"
+    SELECT * FROM penjualan WHERE id_jual='$id_jual'
+");
+$l = mysqli_fetch_assoc($lama);
+
+$barang_lama = mysqli_query($koneksi,"
+    SELECT * FROM barang WHERE id_barang='".$l['id_barang']."'
+");
+$bl = mysqli_fetch_assoc($barang_lama);
+
+$stok_kembali = $bl['stok'] + ($l['total_harga'] / $bl['harga_jual']);
+mysqli_query($koneksi,"
+    UPDATE barang SET stok='$stok_kembali'
+    WHERE id_barang='".$l['id_barang']."'
+");
+
+$barang = mysqli_query($koneksi,"
+    SELECT * FROM barang WHERE id_barang='$id_barang'
+");
+$b = mysqli_fetch_assoc($barang);
+
+if($jumlah > $b['stok']){
+    echo "<script>
+        alert('Stok tidak mencukupi!');
+        window.location='penjualan_edit.php?id=$id_jual';
+    </script>";
+    exit;
+}
+
+$total_harga = $b['harga_jual'] * $jumlah;
+
+$stok_baru = $b['stok'] - $jumlah;
+mysqli_query($koneksi,"
+    UPDATE barang SET stok='$stok_baru'
+    WHERE id_barang='$id_barang'
+");
+
+mysqli_query($koneksi,"
+    UPDATE penjualan SET
+        id_barang='$id_barang',
+        user_id='$user_id',
+        total_harga='$total_harga',
+        tgl_jual='$tgl_jual'
     WHERE id_jual='$id_jual'
 ");
 
